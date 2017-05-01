d47aeb095383f5d04c1372b7f3f7aece1a9a0657
Valle1337 <valentinschuchi@aol.com>
2017-05-01 15:18:57 +0200

    initial upload
    is-state of Nachtwerx 5.x

---
+++ Arma3Script.bat
@@ -0,0 +1,3 @@
+cd "C:\Servers\Nachtwerk 5.0"
+
+start arma3server.exe -mod=@extDB3;@life_server;@infiSTAR_A3; -port=2312 -config=server.cfg -cfg=basic.cfg -enableHT -maxMem=3071 -noFilePatching -noSplash -noSound -autoInit -loadMissionToMemory -netlog
