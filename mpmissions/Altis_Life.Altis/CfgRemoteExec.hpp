de2abf19b18f456e7eb7c7d7b158c19b163311ac
Valle1337 <valentin.schuchmann@aol.de>
2017-05-23 17:54:08 +0200

    ohne spyglass

--- mpmissions/Altis_Life.Altis/CfgRemoteExec.hpp
+++ mpmissions/Altis_Life.Altis/CfgRemoteExec.hpp
@@ -81,8 +81,6 @@ class CfgRemoteExec {
         F(life_fnc_wantedFetch,SERVER)
         F(life_fnc_wantedProfUpdate,SERVER)
         F(life_fnc_wantedRemove,SERVER)
-        //F(SPY_fnc_cookieJar,SERVER)
-        //F(SPY_fnc_observe,SERVER)
         F(TON_fnc_addContainer,SERVER)
         F(TON_fnc_addHouse,SERVER)
         F(TON_fnc_chopShopSell,SERVER)
@@ -160,7 +158,6 @@ class CfgRemoteExec {
         F(life_fnc_say3D,ANYONE)
         F(life_fnc_setFuel,ANYONE)
         F(life_fnc_simDisable,ANYONE)
-        //F(SPY_fnc_notifyAdmins,ANYONE)
 		F(bf_fnc_lightsOn,ANYONE)
     };
 
