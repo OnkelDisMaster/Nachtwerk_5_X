ca5606ff3e7b4a21fb2ff110df8036d9930d7653
jumpski <daniel.dziamski@gmx.de>
2017-06-17 23:14:53 +0200

    change cellphone

--- mpmissions/Altis_Life.Altis/dialog/player_inv.hpp
+++ mpmissions/Altis_Life.Altis/dialog/player_inv.hpp
@@ -266,7 +266,7 @@ class playerSettings {
  y = 0.7016 * safezoneH + safezoneY;
  w = 0.028875 * safezoneW;
  h = 0.0504 * safezoneH;
- onButtonClick = "createDialog ""Life_cell_phone"";";
+ onButtonClick = "createDialog ""Life_my_smartphone"";";
  tooltip = $STR_PM_CellPhone;
  colorBackground[] = {-1,-1,-1,-1};
  colorBackgroundFocused[] = {1,1,1,0.12};
