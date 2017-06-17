b2f54ce13c2c2b6e95a9f642ac237265a1d70eb2
jumpski <daniel.dziamski@gmx.de>
2017-06-17 22:06:12 +0200

    bugfix wanted list

--- mpmissions/Altis_Life.Altis/dialog/wanted_list.hpp
+++ mpmissions/Altis_Life.Altis/dialog/wanted_list.hpp
@@ -48,7 +48,7 @@ class life_wanted_menu {
 			idc = 2401;
 			text = "";
 			sizeEx = 0.035;
-			onLBSelChanged = "[] call life_fnc_wantedGrab";
+			onLBSelChanged = "[_this] call life_fnc_wantedInfo";
 
 			x = 0.12; y = 0.26;
 			w = 0.2; h = 0.7;
