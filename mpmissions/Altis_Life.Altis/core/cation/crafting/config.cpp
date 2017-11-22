a0384282c4a54f8cce545651a33f8a8b1b228126
Valle1337 <valentin.schuchmann@aol.de>
2017-11-23 00:08:09 +0100

    map

--- mpmissions/Altis_Life.Altis/core/cation/crafting/config.cpp
+++ mpmissions/Altis_Life.Altis/core/cation/crafting/config.cpp
@@ -111,8 +111,7 @@ class Cation_Crafting {
 															// 2 * 50			+ 3 * 8				+ 5 * 4		
 			{"implantatPaycheck","license_civ_implantat",{"implantatNormal",2,"mikroprozessor",3,"brennstab",5},"","",1},
 															// 2 * 50			+ 5 * 8	
-			//{"implantatBackpack","license_civ_implantat",{"implantatNormal",2,"mikroprozessor",5},"","",1}
-			 {"implantatBackpack","",{"alu",10,"eisen",10},"","",1}
+			{"implantatBackpack","license_civ_implantat",{"implantatNormal",2,"mikroprozessor",5},"","",1}
         },
 		{
             "bombenkopfe",
