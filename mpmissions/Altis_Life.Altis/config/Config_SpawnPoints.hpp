c6bff374e293fca47bbe94a013bb6eeb555dcb6d
Valle1337 <valentin.schuchmann@aol.de>
2018-08-20 16:23:11 +0200

    cfg spawns

--- mpmissions/Altis_Life.Altis/config/Config_SpawnPoints.hpp
+++ mpmissions/Altis_Life.Altis/config/Config_SpawnPoints.hpp
@@ -9,129 +9,131 @@
 *
 */
 class CfgSpawnPoints {
-    class Civilian {
-        class Kavala {
-            displayName = "Kavala";
-            spawnMarker = "civ_spawn_1";
-            icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
-            conditions = "";
+    class Altis {
+        class Civilian {
+            class Kavala {
+                displayName = "Kavala";
+                spawnMarker = "civ_spawn_1";
+                icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
+                conditions = "";
+            };
+
+            class Athira {
+                displayName = "Athira";
+                spawnMarker = "civ_spawn_3";
+                icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
+                conditions = "";
+            };
+
+            class Pyrgos {
+                displayName = "Pyrgos";
+                spawnMarker = "civ_spawn_2";
+                icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
+                conditions = "";
+            };
+
+            class Sofia {
+                displayName = "Sofia";
+                spawnMarker = "civ_spawn_4";
+                icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
+                conditions = "";
+            };
+
+            class RebelS {
+                displayName = "REB HQ";
+                spawnMarker = "reb_spawn_1";
+                icon = "\a3\ui_f\data\map\MapControl\bunker_ca.paa";
+                conditions = "license_civ_rebel || license_civ_pro_rebel";
+            };
+
+            class RebelE {
+                displayName = "Rebellen Außenposten";
+                spawnMarker = "reb_spawn_3";
+                icon = "\a3\ui_f\data\map\MapControl\bunker_ca.paa";
+                conditions = "license_civ_rebel || license_civ_pro_rebel";
+            };
+            
+            class Elite {
+                displayName = "Elite Rebellen HQ";
+                spawnMarker = "elite_spawn";
+                icon = "\a3\ui_f\data\map\MapControl\bunker_ca.paa";
+                conditions = "license_civ_elite";
+            };
         };
 
-        class Athira {
-            displayName = "Athira";
-            spawnMarker = "civ_spawn_3";
-            icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
-            conditions = "";
+        class Cop {
+            class Kavala {
+                displayName = "Kavala HQ";
+                spawnMarker = "cop_spawn_1";
+                icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
+                conditions = "playerSide isEqualTo west";
+            };
+
+            class Athira {
+                displayName = "Athira HQ";
+                spawnMarker = "cop_spawn_3";
+                icon = "\a3\ui_f\data\map\MapControl\fuelstation_ca.paa";
+                conditions = "playerSide isEqualTo west";
+            };
+
+            class Pyrgos {
+                displayName = "Pyrgos HQ";
+                spawnMarker = "cop_spawn_2";
+                icon = "\a3\ui_f\data\map\GroupIcons\badge_rotate_0_gs.paa";
+                conditions = "playerSide isEqualTo west";
+            };
+            
+            class ubung {
+                displayName = "Schießplatz";
+                spawnMarker = "cop_spawn_6";
+                icon = "\a3\ui_f\data\map\GroupIcons\badge_rotate_0_gs.paa";
+                conditions = "playerSide isEqualTo west";
+            };
+
+            class Air {
+                displayName = "Gefängnis";
+                spawnMarker = "cop_spawn_4";
+                icon = "\a3\ui_f\data\map\Markers\NATO\b_air.paa";
+                conditions = "playerSide isEqualTo west && call life_coplevel >= 3";
+            };
+
+            class HW {
+                displayName = "SEK HQ";
+                spawnMarker = "cop_spawn_5";
+                icon = "\a3\ui_f\data\map\GroupIcons\badge_rotate_0_gs.paa";
+                conditions = "playerSide isEqualTo west && call life_coplevel >= 9";
+            };
+
         };
 
-        class Pyrgos {
-            displayName = "Pyrgos";
-            spawnMarker = "civ_spawn_2";
-            icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
-            conditions = "";
-        };
+        class Medic {
+            class Kavala {
+                displayName = "Kavala Hospital";
+                spawnMarker = "medic_spawn_1";
+                icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
+                conditions = "";
+            };
+            
+            class Airfield {
+                displayName = "Flughafen";
+                spawnMarker = "medic_spawn_4";
+                icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
+                conditions = "";
+            };
 
-        class Sofia {
-            displayName = "Sofia";
-            spawnMarker = "civ_spawn_4";
-            icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
-            conditions = "";
-        };
+            class Athira {
+                displayName = "Sofia Regional";
+                spawnMarker = "medic_spawn_2";
+                icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
+                conditions = "";
+            };
 
-        class RebelS {
-            displayName = "REB HQ";
-            spawnMarker = "reb_spawn_1";
-            icon = "\a3\ui_f\data\map\MapControl\bunker_ca.paa";
-            conditions = "license_civ_rebel || license_civ_pro_rebel";
-        };
-
-        class RebelE {
-            displayName = "Rebellen Außenposten";
-            spawnMarker = "reb_spawn_3";
-            icon = "\a3\ui_f\data\map\MapControl\bunker_ca.paa";
-            conditions = "license_civ_rebel || license_civ_pro_rebel";
-        };
-		
-		class Elite {
-            displayName = "Elite Rebellen HQ";
-            spawnMarker = "elite_spawn";
-            icon = "\a3\ui_f\data\map\MapControl\bunker_ca.paa";
-            conditions = "license_civ_elite";
-        };
-    };
-
-    class Cop {
-        class Kavala {
-            displayName = "Kavala HQ";
-            spawnMarker = "cop_spawn_1";
-            icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
-            conditions = "playerSide isEqualTo west";
-        };
-
-        class Athira {
-            displayName = "Athira HQ";
-            spawnMarker = "cop_spawn_3";
-            icon = "\a3\ui_f\data\map\MapControl\fuelstation_ca.paa";
-            conditions = "playerSide isEqualTo west";
-        };
-
-        class Pyrgos {
-            displayName = "Pyrgos HQ";
-            spawnMarker = "cop_spawn_2";
-            icon = "\a3\ui_f\data\map\GroupIcons\badge_rotate_0_gs.paa";
-            conditions = "playerSide isEqualTo west";
-        };
-		
-		class ubung {
-            displayName = "Schießplatz";
-            spawnMarker = "cop_spawn_6";
-            icon = "\a3\ui_f\data\map\GroupIcons\badge_rotate_0_gs.paa";
-            conditions = "playerSide isEqualTo west";
-        };
-
-        class Air {
-            displayName = "Gefängnis";
-            spawnMarker = "cop_spawn_4";
-            icon = "\a3\ui_f\data\map\Markers\NATO\b_air.paa";
-            conditions = "playerSide isEqualTo west && call life_coplevel >= 3";
-        };
-
-        class HW {
-            displayName = "SEK HQ";
-            spawnMarker = "cop_spawn_5";
-            icon = "\a3\ui_f\data\map\GroupIcons\badge_rotate_0_gs.paa";
-            conditions = "playerSide isEqualTo west && call life_coplevel >= 9";
-        };
-
-    };
-
-    class Medic {
-        class Kavala {
-            displayName = "Kavala Hospital";
-            spawnMarker = "medic_spawn_1";
-            icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
-            conditions = "";
-        };
-		
-		class Airfield {
-            displayName = "Flughafen";
-            spawnMarker = "medic_spawn_4";
-            icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
-            conditions = "";
-        };
-
-        class Athira {
-            displayName = "Sofia Regional";
-            spawnMarker = "medic_spawn_2";
-            icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
-            conditions = "";
-        };
-
-        class Pyrgos {
-            displayName = "Pyrgos Hospital";
-            spawnMarker = "medic_spawn_3";
-            icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
-            conditions = "";
+            class Pyrgos {
+                displayName = "Pyrgos Hospital";
+                spawnMarker = "medic_spawn_3";
+                icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
+                conditions = "";
+            };
         };
     };
 };
