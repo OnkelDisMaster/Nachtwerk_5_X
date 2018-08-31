ef822a888f87738cf5a16d7fc50ca218985e8dbe
Valle1337 <valentin.schuchmann@aol.de>
2018-09-01 01:59:35 +0200

    Start

--- Arma3Script.bat
+++ Arma3Script.bat
@@ -1,3 +1,3 @@
-cd "C:\GameServer\Altis_Life\Nachtwerk_5_X"
+cd "C:\SERVER NACHTWERK\Arma3\Altis_Life\Nachtwerk_5_X"
 
 start arma3server.exe -servermod=@extDB3;@life_server;@infiSTAR_A3;@Sperrzonensystem;@HMCS;@obfusqf; -port=2312 -config=server.cfg -cfg=basic.cfg -enableHT -maxMem=3071 -noFilePatching -noSplash -noSound -autoInit -loadMissionToMemory -netlog
